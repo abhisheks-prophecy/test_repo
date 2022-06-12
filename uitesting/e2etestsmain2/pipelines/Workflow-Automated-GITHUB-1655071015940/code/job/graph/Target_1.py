ec27514a9ade498f45ecb2d8ac1cfab6529d40d3
abhisheks+e2etests@simpledatalabs.com <abhisheks+e2etests@simpledatalabs.com>
2022-06-12 21:59:15 +0000

    Simple workflow Squash and Rebase

---
+++ uitesting/e2etestsmain2/pipelines/Workflow-Automated-GITHUB-1655071015940/code/job/graph/Target_1.py
@@ -0,0 +1,17 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from job.config.ConfigStore import *
+from job.udfs.UDFs import *
+
+def Target_1(spark: SparkSession, in0: DataFrame):
+    if Config.fabricName == "dev":
+        in0.write\
+            .option("header", True)\
+            .option("sep", ",")\
+            .mode("overwrite")\
+            .option("separator", ",")\
+            .option("header", True)\
+            .csv("dbfs:/tmp/e2e/dataset_out_52690")
+    else:
+        raise Exception("No valid dataset present to read fabric")
