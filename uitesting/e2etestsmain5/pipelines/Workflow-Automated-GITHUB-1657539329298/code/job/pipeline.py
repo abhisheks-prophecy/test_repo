720bd937da3d21eee677438c57d2768325b1cde7
abhisheks+e2etests@simpledatalabs.com <abhisheks+e2etests@simpledatalabs.com>
2022-07-14 23:08:09 +0000

    Simple workflow Squash and Rebase

---
+++ uitesting/e2etestsmain5/pipelines/Workflow-Automated-GITHUB-1657539329298/code/job/pipeline.py
@@ -0,0 +1,28 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from job.config.ConfigStore import *
+from job.udfs.UDFs import *
+from prophecy.utils import *
+from job.graph import *
+
+def pipeline(spark: SparkSession) -> None:
+    df_dataset_cust_in = dataset_cust_in(spark)
+    df_Filter_1 = Filter_1(spark, df_dataset_cust_in)
+    dataset_out_27365(spark, df_Filter_1)
+
+def main():
+    spark = SparkSession.builder\
+                .config("spark.default.parallelism", "4")\
+                .config("spark.sql.legacy.allowUntypedScalaUDF", "true")\
+                .enableHiveSupport()\
+                .appName("Prophecy Pipeline")\
+                .getOrCreate()
+    Utils.initializeFromArgs(spark, parse_args())
+    spark.conf.set("prophecy.metadata.pipeline.uri", "7834/pipelines/Workflow-Automated-GITHUB-1657539329298")
+    MetricsCollector.start(spark)
+    pipeline(spark)
+    MetricsCollector.end(spark)
+
+if __name__ == "__main__":
+    main()
