689a31b1bed817882ae9e15428aa2c9f2096da2f
abhisheks+e2etests1@simpledatalabs.com <abhisheks+e2etests1@simpledatalabs.com>
2022-08-22 17:53:53 +0000

    Simple workflow Squash and Rebase

---
+++ test/e2etestsmain5/pipelines/Workflow-Automated-GITHUB-1658684302527/code/job/graph/__init__.py
@@ -0,0 +1,3 @@
+from .dataset_out_56975 import dataset_out_56975
+from .dataset_cust_in import dataset_cust_in
+from .Filter_1 import Filter_1
---
+++ uitesting/e2etestsmain5/pipelines/Workflow-Automated-GITHUB-1658789001720/code/job/graph/__init__.py
@@ -0,0 +1,3 @@
+from .dataset_out_26568 import dataset_out_26568
+from .dataset_cust_in import dataset_cust_in
+from .Filter_1 import Filter_1
