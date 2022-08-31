c36184f09dd695852984381f50845975e84c5c00
abhisheks+e2etests1@simpledatalabs.com <abhisheks+e2etests1@simpledatalabs.com>
2022-08-31 17:31:04 +0000

    Simple workflow Squash and Rebase

---
+++ test/e2etestsmain5/pipelines/Workflow-Automated-GITHUB-1658079854785/code/job/graph/__init__.py
@@ -0,0 +1,3 @@
+from .dataset_cust_in import dataset_cust_in
+from .dataset_out_47636 import dataset_out_47636
+from .Filter_1 import Filter_1
---
+++ test/e2etestsmain5/pipelines/Workflow-Automated-GITHUB-1659030646074/code/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'Workflow-Automated-GITHUB-1659030646074',
+    version = '1.0',
+    packages = find_packages(include = ('workflowautomatedgithub*', )),
+    description = 'workflow',
+    install_requires = [
+'prophecy-libs==1.1.14'],
+    entry_points = {
+'console_scripts' : [
+'main = workflowautomatedgithub.pipeline:main', ], },
+    extras_require = {
+'test' : ['pytest', 'pytest-html'], }
+)
