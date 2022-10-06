c66a6471d6b9bd3b3bad604f19c6beee2192a39f
abhisheks@prophecy.io <abhisheks@prophecy.io>
2022-10-06 07:46:26 +0000

    ❇️ New     - Pipeline        - TestNew1

---
+++ test/ayush.dev/python/pipelines/TestNew1/code/testnew1/graph/__init__.py
@@ -0,0 +1,6 @@
+from .customers_orders import *
+from .Reformat_3 import *
+from .src_avro import *
+from .Reformat_1 import *
+from .TEST_SCNEARIO1 import *
+from .Reformat_2 import *
---
+++ test/ayush.dev/python/pipelines/TestNew1/code/testnew1/pipeline.py
@@ -0,0 +1,33 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from testnew1.config.ConfigStore import *
+from testnew1.udfs.UDFs import *
+from prophecy.utils import *
+from testnew1.graph import *
+
+def pipeline(spark: SparkSession) -> None:
+    df_customers_orders = customers_orders(spark)
+    df_src_avro = src_avro(spark)
+    df_Reformat_1 = Reformat_1(spark, df_src_avro)
+    df_Reformat_2 = Reformat_2(spark, df_customers_orders)
+    df_TEST_SCNEARIO1 = TEST_SCNEARIO1(spark)
+    df_Reformat_3 = Reformat_3(spark, df_TEST_SCNEARIO1)
+
+def main():
+    spark = SparkSession.builder\
+                .config("spark.default.parallelism", "4")\
+                .config("spark.sql.legacy.allowUntypedScalaUDF", "true")\
+                .enableHiveSupport()\
+                .appName("Prophecy Pipeline")\
+                .getOrCreate()\
+                .newSession()
+    Utils.initializeFromArgs(spark, parse_args())
+    spark.conf.set("prophecy.metadata.pipeline.uri", "pipelines/TestNew1")
+    
+    MetricsCollector.start(spark = spark, pipelineId = spark.conf.get("prophecy.project.id") + "/" + "pipelines/TestNew1")
+    pipeline(spark)
+    MetricsCollector.end(spark)
+
+if __name__ == "__main__":
+    main()
