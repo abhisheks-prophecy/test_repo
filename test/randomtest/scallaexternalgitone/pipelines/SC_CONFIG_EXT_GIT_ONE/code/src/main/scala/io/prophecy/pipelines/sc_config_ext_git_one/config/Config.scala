2a4a60b8e00a88a838b61c5412ab3a819fb11144
abhisheks@prophecy.io <abhisheks@prophecy.io>
2022-11-12 05:51:32 +0000

    ❇️ New     - Subgraph        - testsubgraph1     - Dataset        - csv_special_char        - qa_database_all_type_parquet        - prophecy_component_runs     - Pipeline        - SC_CONFIG_EXT_GIT_ONE

---
+++ test/randomtest/scallaexternalgitone/pipelines/SC_CONFIG_EXT_GIT_ONE/code/src/main/scala/io/prophecy/pipelines/sc_config_ext_git_one/config/Config.scala
@@ -0,0 +1,37 @@
+package io.prophecy.pipelines.sc_config_ext_git_one.config
+
+import io.prophecy.pipelines.sc_config_ext_git_one.config.ConfigStore._
+import pureconfig._
+import io.prophecy.libs._
+
+case class Config(
+  c_string: String = "stringhoonmain#@!%^&*2asda",
+  c_int:    Int = -12312,
+  c_float:  Float = 231.12312f,
+  c_double: Double = 4.23423432412e11d,
+  c_short:  Short = 123,
+  c_databricks_secrets: DatabricksSecret =
+    DatabricksSecret(scope = "qasecrets_mysql", key = "username"),
+  c_spark_expression: String = "concat(`c  date`, `c  float`)",
+  c_boolean:          Boolean = false,
+  `c-ancient-config`: String = "arent i"
+) extends ConfigBase
+
+object DatabricksSecret {
+
+  implicit val myIntReader: ConfigReader[DatabricksSecret] =
+    ConfigReader[String].map { s =>
+      val Array(scope, key) = s.split(":")
+      DatabricksSecret(scope, key)
+    }
+
+}
+
+case class DatabricksSecret(scope: String, key: String) {
+
+  override def toString: String = {
+    import com.databricks.dbutils_v1.DBUtilsHolder.dbutils
+    dbutils.secrets.get(scope = scope, key = key)
+  }
+
+}
---
+++ test/randomtest/scallaexternalgitone/subgraphs/testsubgraph1/code/src/main/scala/org/scala/testsubgraph1/pipelines/sc_config_ext_git_one/graph/Subgraph_1/Limit_1.scala
@@ -0,0 +1,16 @@
+package org.scala.testsubgraph1.pipelines.sc_config_ext_git_one.graph.Subgraph_1
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.sc_config_ext_git_one.config.ConfigStore._
+import io.prophecy.pipelines.sc_config_ext_git_one.udfs.UDFs._
+import io.prophecy.pipelines.sc_config_ext_git_one.udfs._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Limit_1 {
+  def apply(spark: SparkSession, in: DataFrame): DataFrame = in.limit(10)
+}
