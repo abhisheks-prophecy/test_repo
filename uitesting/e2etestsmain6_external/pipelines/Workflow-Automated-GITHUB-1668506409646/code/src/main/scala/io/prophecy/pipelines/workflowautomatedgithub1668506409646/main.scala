e8150137f795890fbaa1da89329005310df48b74
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2022-11-15 10:01:18 +0000

    Simple workflow Commit

---
+++ uitesting/e2etestsmain6_external/pipelines/Workflow-Automated-GITHUB-1668506409646/code/src/main/scala/io/prophecy/pipelines/workflowautomatedgithub1668506409646/main.scala
@@ -0,0 +1,45 @@
+package io.prophecy.pipelines.workflowautomatedgithub1668506409646
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.workflowautomatedgithub1668506409646.config.ConfigStore._
+import io.prophecy.pipelines.workflowautomatedgithub1668506409646.config._
+import io.prophecy.pipelines.workflowautomatedgithub1668506409646.udfs.UDFs._
+import io.prophecy.pipelines.workflowautomatedgithub1668506409646.udfs._
+import io.prophecy.pipelines.workflowautomatedgithub1668506409646.graph._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Main {
+
+  def apply(spark: SparkSession): Unit = {
+    val df_Source_0 = Source_0(spark)
+  }
+
+  def main(args: Array[String]): Unit = {
+    ConfigStore.Config = ConfigurationFactoryImpl.fromCLI(args)
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("Prophecy Pipeline")
+      .config("spark.default.parallelism",             "4")
+      .config("spark.sql.legacy.allowUntypedScalaUDF", "true")
+      .enableHiveSupport()
+      .getOrCreate()
+      .newSession()
+    spark.conf.set("prophecy.metadata.pipeline.uri",
+                   "pipelines/Workflow-Automated-GITHUB-1668506409646"
+    )
+    MetricsCollector.start(
+      spark,
+      spark.conf.get(
+        "prophecy.project.id"
+      ) + "/" + "pipelines/Workflow-Automated-GITHUB-1668506409646"
+    )
+    apply(spark)
+    MetricsCollector.end(spark)
+  }
+
+}
