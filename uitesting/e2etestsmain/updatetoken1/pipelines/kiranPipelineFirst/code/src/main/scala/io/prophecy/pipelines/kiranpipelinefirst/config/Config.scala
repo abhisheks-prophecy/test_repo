a7c2ad1087676db9959f9dbe147df7ba970b3d7e
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2022-11-15 11:08:23 +0000

    commit Kiran devfirst

---
+++ uitesting/e2etestsmain/updatetoken1/pipelines/kiranPipelineFirst/code/src/main/scala/io/prophecy/pipelines/kiranpipelinefirst/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.kiranpipelinefirst.config
+
+import io.prophecy.pipelines.kiranpipelinefirst.config.ConfigStore._
+import pureconfig._
+import io.prophecy.libs._
+case class Config() extends ConfigBase
