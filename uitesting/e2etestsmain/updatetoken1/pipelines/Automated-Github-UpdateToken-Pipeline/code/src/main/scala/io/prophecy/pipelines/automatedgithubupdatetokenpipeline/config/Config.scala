0b9bf9cb6fb1d89f41c2136341b917e2024f2368
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2022-12-01 21:33:37 +0000

    Simple workflow Commit

--- uitesting/e2etestsmain/updatetoken1/pipelines/Automated-Github-UpdateToken-Pipeline/code/src/main/scala/io/prophecy/pipelines/automatedgithubupdatetokenpipeline/config/Config.scala
+++ uitesting/e2etestsmain/updatetoken1/pipelines/Automated-Github-UpdateToken-Pipeline/code/src/main/scala/io/prophecy/pipelines/automatedgithubupdatetokenpipeline/config/Config.scala
@@ -2,5 +2,6 @@ package io.prophecy.pipelines.automatedgithubupdatetokenpipeline.config
 
 import io.prophecy.pipelines.automatedgithubupdatetokenpipeline.config.ConfigStore._
 import pureconfig._
+import pureconfig.generic.ProductHint
 import io.prophecy.libs._
 case class Config() extends ConfigBase
