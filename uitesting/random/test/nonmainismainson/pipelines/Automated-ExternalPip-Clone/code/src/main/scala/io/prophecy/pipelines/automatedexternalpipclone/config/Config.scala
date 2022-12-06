c0770f87877295ae61c886938f0ca51957e57f17
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2022-12-06 12:23:47 +0000

    ❇️ New     - Pipeline        - Automated-ExternalPip-Clone     - Dataset        - Automated-ExternalDataset-Clone     - Job        - Automated-ExternalJob-Clone     - Subgraph        - Automated-ExternalSG-Clone

---
+++ uitesting/random/test/nonmainismainson/pipelines/Automated-ExternalPip-Clone/code/src/main/scala/io/prophecy/pipelines/automatedexternalpipclone/config/Config.scala
@@ -0,0 +1,7 @@
+package io.prophecy.pipelines.automatedexternalpipclone.config
+
+import io.prophecy.pipelines.automatedexternalpipclone.config.ConfigStore._
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
