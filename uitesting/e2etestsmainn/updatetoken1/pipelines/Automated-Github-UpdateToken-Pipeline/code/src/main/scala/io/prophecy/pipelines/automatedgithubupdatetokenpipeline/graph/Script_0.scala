8ed315883d2fbda184276aa610ca63334a477ec3
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2022-12-08 12:03:43 +0000

    Simple workflow Commit

---
+++ uitesting/e2etestsmainn/updatetoken1/pipelines/Automated-Github-UpdateToken-Pipeline/code/src/main/scala/io/prophecy/pipelines/automatedgithubupdatetokenpipeline/graph/Script_0.scala
@@ -0,0 +1,20 @@
+package io.prophecy.pipelines.automatedgithubupdatetokenpipeline.graph
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.automatedgithubupdatetokenpipeline.config.ConfigStore._
+import io.prophecy.pipelines.automatedgithubupdatetokenpipeline.udfs.UDFs._
+import io.prophecy.pipelines.automatedgithubupdatetokenpipeline.udfs._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Script_0 {
+  def apply(spark: SparkSession, in0: DataFrame): DataFrame = {
+    
+    out0
+  }
+
+}
