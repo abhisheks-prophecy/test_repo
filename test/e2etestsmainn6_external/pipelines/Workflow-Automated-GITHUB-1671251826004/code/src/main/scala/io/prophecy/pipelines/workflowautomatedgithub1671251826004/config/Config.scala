a28e77d98f156dee884ffbff1f279b332d366d71
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2022-12-17 04:38:02 +0000

    Simple workflow Commit

---
+++ test/e2etestsmainn6_external/pipelines/Workflow-Automated-GITHUB-1671251826004/code/src/main/scala/io/prophecy/pipelines/workflowautomatedgithub1671251826004/config/Config.scala
@@ -0,0 +1,8 @@
+package io.prophecy.pipelines.workflowautomatedgithub1671251826004.config
+
+import io.prophecy.pipelines.workflowautomatedgithub1671251826004.config.ConfigStore._
+import io.prophecy.pipelines.workflowautomatedgithub1671251826004.config.Context
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
---
+++ test/e2etestsmainn6_external/pipelines/Workflow-Automated-GITHUB-1671251826004/code/src/main/scala/io/prophecy/pipelines/workflowautomatedgithub1671251826004/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.workflowautomatedgithub1671251826004.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
