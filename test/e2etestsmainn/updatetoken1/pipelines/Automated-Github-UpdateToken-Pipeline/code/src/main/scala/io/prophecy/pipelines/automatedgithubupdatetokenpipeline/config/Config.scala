c0efbe0a0108d23cf2c67433dc1e1cec6fc19a88
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2022-12-17 04:58:14 +0000

    Simple workflow Commit

---
+++ test/e2etestsmainn/updatetoken1/pipelines/Automated-Github-UpdateToken-Pipeline/code/src/main/scala/io/prophecy/pipelines/automatedgithubupdatetokenpipeline/config/Config.scala
@@ -0,0 +1,8 @@
+package io.prophecy.pipelines.automatedgithubupdatetokenpipeline.config
+
+import io.prophecy.pipelines.automatedgithubupdatetokenpipeline.config.ConfigStore._
+import io.prophecy.pipelines.automatedgithubupdatetokenpipeline.config.Context
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
---
+++ test/e2etestsmainn/updatetoken1/pipelines/Automated-Github-UpdateToken-Pipeline/code/src/main/scala/io/prophecy/pipelines/automatedgithubupdatetokenpipeline/graph/Script_0.scala
@@ -0,0 +1,23 @@
+package io.prophecy.pipelines.automatedgithubupdatetokenpipeline.graph
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.automatedgithubupdatetokenpipeline.config.ConfigStore._
+import io.prophecy.pipelines.automatedgithubupdatetokenpipeline.config.Context
+import io.prophecy.pipelines.automatedgithubupdatetokenpipeline.udfs.UDFs._
+import io.prophecy.pipelines.automatedgithubupdatetokenpipeline.udfs._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Script_0 {
+  def apply(context: Context, in0: DataFrame): DataFrame = {
+    val spark = context.spark
+    val Config = context.config
+    
+    out0
+  }
+
+}
