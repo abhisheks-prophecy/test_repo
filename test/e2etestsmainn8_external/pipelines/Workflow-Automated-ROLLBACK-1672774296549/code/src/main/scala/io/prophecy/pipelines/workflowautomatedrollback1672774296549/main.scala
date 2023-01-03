12a945b50bab0a9861a397df1ef37f3aec814b0d
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2023-01-03 19:33:40 +0000

    Simple workflow Commit

--- test/e2etestsmainn8_external/pipelines/Workflow-Automated-ROLLBACK-1672774296549/code/src/main/scala/io/prophecy/pipelines/workflowautomatedrollback1672774296549/main.scala
+++ test/e2etestsmainn8_external/pipelines/Workflow-Automated-ROLLBACK-1672774296549/code/src/main/scala/io/prophecy/pipelines/workflowautomatedrollback1672774296549/main.scala
@@ -19,6 +19,7 @@ object Main {
   def apply(context: Context): Unit = {
     val df_Source_1 = Source_1(context)
     val df_Source_0 = Source_0(context)
+    val df_Source_2 = Source_2(context)
   }
 
   def main(args: Array[String]): Unit = {
