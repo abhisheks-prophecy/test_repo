1cad0f5c397a2c68497c927a0d0b4899b762a33f
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2023-01-18 18:48:07 +0000

    Simple workflow Commit

---
+++ uitesting/e2etestsmainn8_external/pipelines/Workflow-Automated-ROLLBACK-1674067625057/code/src/main/scala/io/prophecy/pipelines/workflowautomatedrollback1674067625057/config/Config.scala
@@ -0,0 +1,8 @@
+package io.prophecy.pipelines.workflowautomatedrollback1674067625057.config
+
+import io.prophecy.pipelines.workflowautomatedrollback1674067625057.config.ConfigStore._
+import io.prophecy.pipelines.workflowautomatedrollback1674067625057.config.Context
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
---
+++ uitesting/e2etestsmainn8_external/pipelines/Workflow-Automated-ROLLBACK-1674067625057/code/src/main/scala/io/prophecy/pipelines/workflowautomatedrollback1674067625057/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.workflowautomatedrollback1674067625057.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
