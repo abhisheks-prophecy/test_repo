04cf488e7293574df20d6ba1cb272c566d0ec942
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2023-01-24 16:51:21 +0000

    Simple workflow Commit

---
+++ test/e2etestsmainn8_external/pipelines/Workflow-Automated-ROLLBACK-1674579027788/code/src/main/scala/io/prophecy/pipelines/workflowautomatedrollback1674579027788/config/Config.scala
@@ -0,0 +1,8 @@
+package io.prophecy.pipelines.workflowautomatedrollback1674579027788.config
+
+import io.prophecy.pipelines.workflowautomatedrollback1674579027788.config.ConfigStore._
+import io.prophecy.pipelines.workflowautomatedrollback1674579027788.config.Context
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
