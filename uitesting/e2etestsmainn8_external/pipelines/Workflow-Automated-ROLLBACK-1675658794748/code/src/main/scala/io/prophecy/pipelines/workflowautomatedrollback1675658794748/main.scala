78a3531d5dd33057dc42e0d228e52d741d0e9014
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2023-02-06 04:47:53 +0000

    Simple workflow Commit

---
+++ uitesting/e2etestsmainn8_external/pipelines/Workflow-Automated-ROLLBACK-1675658794748/code/src/main/scala/io/prophecy/pipelines/workflowautomatedrollback1675658794748/main.scala
@@ -0,0 +1,44 @@
+package io.prophecy.pipelines.workflowautomatedrollback1675658794748
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.workflowautomatedrollback1675658794748.config.ConfigStore._
+import io.prophecy.pipelines.workflowautomatedrollback1675658794748.config.Context
+import io.prophecy.pipelines.workflowautomatedrollback1675658794748.config._
+import io.prophecy.pipelines.workflowautomatedrollback1675658794748.udfs.UDFs._
+import io.prophecy.pipelines.workflowautomatedrollback1675658794748.udfs._
+import io.prophecy.pipelines.workflowautomatedrollback1675658794748.graph._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Main {
+
+  def apply(context: Context): Unit = {
+    val df_Source_0 = Source_0(context)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val config = ConfigurationFactoryImpl.fromCLI(args)
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("Prophecy Pipeline")
+      .config("spark.default.parallelism",             "4")
+      .config("spark.sql.legacy.allowUntypedScalaUDF", "true")
+      .enableHiveSupport()
+      .getOrCreate()
+      .newSession()
+    val context = Context(spark, config)
+    spark.conf.set("prophecy.metadata.pipeline.uri",
+                   "pipelines/Workflow-Automated-ROLLBACK-1675658794748"
+    )
+    MetricsCollector.start(spark,
+                           "pipelines/Workflow-Automated-ROLLBACK-1675658794748"
+    )
+    apply(context)
+    MetricsCollector.end(spark)
+  }
+
+}
