ed160a6171887ddabb40c21993121446cac6f206
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2023-02-09 09:25:26 +0000

    Simple workflow Commit

---
+++ uitesting/e2etestsmainn8_external/pipelines/Workflow-Automated-ROLLBACK-1675934659252/code/src/main/scala/io/prophecy/pipelines/workflowautomatedrollback1675934659252/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.workflowautomatedrollback1675934659252.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
---
+++ uitesting/e2etestsmainn8_external/pipelines/Workflow-Automated-ROLLBACK-1675934659252/code/src/main/scala/io/prophecy/pipelines/workflowautomatedrollback1675934659252/udfs/udafs.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomatedrollback1675934659252.udfs
+
+import org.apache.spark.sql.expressions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+object UDAFs { def registerUDAFs(spark: SparkSession): Unit = {} }
