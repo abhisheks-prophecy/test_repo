2e2a85aa5156919dc7ec74f4b47725bed87c2f91
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2023-03-01 20:10:34 +0000

    Simple workflow Commit

---
+++ uitesting/e2etestsmainn6_external/pipelines/Workflow-Automated-GITHUB-1677701305648/code/src/main/scala/io/prophecy/pipelines/workflowautomatedgithub1677701305648/config/Config.scala
@@ -0,0 +1,8 @@
+package io.prophecy.pipelines.workflowautomatedgithub1677701305648.config
+
+import io.prophecy.pipelines.workflowautomatedgithub1677701305648.config.ConfigStore._
+import io.prophecy.pipelines.workflowautomatedgithub1677701305648.config.Context
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
---
+++ uitesting/e2etestsmainn6_external/pipelines/Workflow-Automated-GITHUB-1677701305648/code/src/main/scala/io/prophecy/pipelines/workflowautomatedgithub1677701305648/udfs/udafs.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomatedgithub1677701305648.udfs
+
+import org.apache.spark.sql.expressions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+object UDAFs { def registerUDAFs(spark: SparkSession): Unit = {} }
