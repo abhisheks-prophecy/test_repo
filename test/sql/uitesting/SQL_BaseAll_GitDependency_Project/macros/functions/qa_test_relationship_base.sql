e6db87540087bd77457a75c25e704a3ad61af397
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-04-15 16:28:37 +0000

    🔁 Modified     - Function        - qa_get_unique_count_base        - qa_all_null_base        - _qa_all_null_base        - qa_test_relationship_base        - qa_epl_data_macro_base        - qa_model_all_above_given_id_base        - qa_all_not_null_base        - qa_complex_macro_base

--- test/sql/uitesting/SQL_BaseAll_GitDependency_Project/macros/functions/qa_test_relationship_base.sql
+++ test/sql/uitesting/SQL_BaseAll_GitDependency_Project/macros/functions/qa_test_relationship_base.sql
@@ -1,4 +1,11 @@
-{% macro qa_test_relationship_base(model1, model2, model1_col, model2_col) %}
+{% macro _qa_all_null_base(model='customers', column_name='id') %}
+
+
+select * from {{ model }} where {{ column_name }} is not null
+{% endmacro %}
+
+ {% macro qa_test_relationship_base(model1, model2, model1_col, model2_col) %}
+
 select count(*)
 from (
     select {{ model1_col }} as id from {{ model }}
@@ -10,22 +17,45 @@ where child.id is not null
   and parent.id is null
 {% endmacro %}
 
- {% macro qa_model_all_above_given_id_base(model, col, id_min=2) %}
+ {% macro qa_epl_data_macro_base(table_name, football_clubs=['Man United', 'Liverpool', 'Man City']) %}
 
-SELECT * from {{model}} where {{col}} > {{ id_min }}
-{% endmacro %}
+{% set status = ['HomeTeam_1','AwayTeam_1'] %}
 
- {% macro qa_all_null_base(model='customers', column_name='id') %}
+with summary as (
+{% for club in football_clubs %}
+    {% for st in status %}
+    select 
+        {{ st }} as team,
+        {% if st == 'HomeTeam' %}
+                case 
+                    when FTR_1 = 'H' then 3
+                    when FTR_1 = 'D' then 1
+                    else 0 end points
+        {% else %}
+                case 
+                    when FTR_1 = 'A' then 3
+                    when FTR_1 = 'D' then 1
+                    else 0 end points
+        {% endif %}
+    from {{ table_name }}
+    where {{ st }} = '{{ club }}'
+        {% if not loop.last %} UNION ALL {% endif %}
+    {% endfor %}
+    {% if not loop.last %} UNION ALL {% endif %}
+{% endfor %}
+)
 
-select * from {{ model }} where {{ column_name }} is not null
-{% endmacro %}
 
- {% macro _qa_all_null_base(model='customers', column_name='id') %}
-
-select * from {{ model }} where {{ column_name }} is not null
+select 
+    team, 
+    sum(points) as total_points
+from summary
+group by team
+order by total_points desc
 {% endmacro %}
 
  {% macro qa_get_unique_count_base(model, column_name) %}
+
 
 select count(*)
 from (
@@ -40,10 +70,12 @@ from (
 
  {% macro qa_all_not_null_base(model, column_name) %}
 
+
 select * from {{ model }} where {{ column_name }} is not null
 {% endmacro %}
 
  {% macro qa_complex_macro_base(model, column_name_int, accepted_values=[1, 2]) %}
+
 
 with all_values as (
     select distinct {{column_name_int}} as col_int from {{model}}
@@ -66,41 +98,16 @@ payments_validation_errors as (
 select * from payments_validation_errors
 {% endmacro %}
 
- 
+ {% macro qa_model_all_above_given_id_base(model, col, id_min=2) %}
 
-{% macro qa_epl_data_macro_base(football_clubs=['Man United','Liverpool','Man City']) %}
-{%- set status = ['HomeTeam','AwayTeam'] -%}
 
-with summary as (
-{% for club in football_clubs -%}
-    {% for st in status -%}
-    select 
-        {{ st }} as team,
-        {% if st == 'HomeTeam' -%}
-                case 
-                    when FTR = 'H' then 3
-                    when FTR = 'D' then 1
-                    else 0 end points
-        {% else -%}
-                case 
-                    when FTR = 'A' then 3
-                    when FTR = 'D' then 1
-                    else 0 end points
-        {% endif -%}
-    from {{ source('staging', 'english-premier-league-table') }}
-    where season = 'season-1819'
-        and {{ st }} = '{{ club }}'
-        {% if not loop.last %} union all {% endif %}
-    {% endfor -%}
-    {% if not loop.last %} union all {% endif %}
-{% endfor -%}
-)
-
--- Calculate the points (aggregation)
-select 
-    team, 
-    sum(points) as total_points
-from summary
-group by team
-order by total_points desc
+SELECT * from {{model}} where {{col}} > {{ id_min }}
 {% endmacro %}
+
+ {% macro qa_all_null_base(model='customers', column_name='id') %}
+
+
+select * from {{ model }} where {{ column_name }} is not null
+{% endmacro %}
+
+ 
