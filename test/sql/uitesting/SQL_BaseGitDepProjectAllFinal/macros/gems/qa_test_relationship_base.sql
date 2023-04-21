e5b99131bf2c4741ed8f8f5faee55bd375e89f8b
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-04-21 09:10:50 +0000

    🔁 Modified     - Function        - qa_epl_data_macro_base        - qa_concat_macro_base        - redshift__language_specific_deduplicate_base        - postgres__language_specific_deduplicate_base        - language_specific_deduplicate_base        - qa_all_null_base        - qa_complex_macro_base        - qa_get_unique_count_base        - qa_model_all_above_given_id_base        - language_specific_concat_base        - _qa_all_null_base        - default__language_specific_deduplicate_base        - bigquery__language_specific_deduplicate_base        - qa_test_relationship_base        - snowflake__language_specific_deduplicate_base        - qa_all_not_null_base     - Model        - model_with_only_seed_base
    ❇️ New
        - Function
           - qa_multi_models_base
           - tpcds_macros_query_1
           - tpcds_macros_query_3
           - tpcds_macros_query_2

--- test/sql/uitesting/SQL_BaseGitDepProjectAllFinal/macros/gems/qa_test_relationship_base.sql
+++ test/sql/uitesting/SQL_BaseGitDepProjectAllFinal/macros/gems/qa_test_relationship_base.sql
@@ -1,26 +1,13 @@
-{% macro _qa_all_null_base(model='customers', column_name='id') %}
+{% macro _qa_all_null_base(model, column_name='id') %}
 
 
 select * from {{ model }} where {{ column_name }} is not null
 {% endmacro %}
 
- {% macro qa_test_relationship_base(model1, model2, model1_col, model2_col) %}
-
-select count(*)
-from (
-    select {{ model1_col }} as id from {{ model }}
-) as child
-left join (
-    select {{ model2_col }} as id from {{ model2 }}
-) as parent on parent.id = child.id
-where child.id is not null
-  and parent.id is null
-{% endmacro %}
-
  {% macro qa_epl_data_macro_base(table_name, football_clubs=['Man United', 'Liverpool', 'Man City']) %}
 
-{% set status = ['HomeTeam_1','AwayTeam_1'] %}
 
+{% set status = ['HomeTeam_1','AwayTeam_1'] %}
 with summary as (
 {% for club in football_clubs %}
     {% for st in status %}
@@ -54,6 +41,39 @@ group by team
 order by total_points desc
 {% endmacro %}
 
+ {% macro qa_test_relationship_base(model1, model2, model1_col, model2_col) %}
+
+
+select count(*)
+from (
+    select {{ model1_col }} as id from {{ model1 }}
+) as child
+left join (
+    select {{ model2_col }} as id from {{ model2 }}
+) as parent on parent.id = child.id
+where child.id is not null
+  and parent.id is null
+{% endmacro %}
+
+ {% macro qa_all_not_null_base(model, column_name) %}
+
+
+select * from {{ model }} where {{ column_name }} is not null
+{% endmacro %}
+
+ {% macro qa_model_all_above_given_id_base(model, col, id_min=2) %}
+
+SELECT * from {{model}} where {{col}} > {{ id_min }}
+{% endmacro %}
+
+ {% macro qa_all_null_base(model='customers', column_name='id') %}
+
+
+
+
+select * from {{ model }} where {{ column_name }} is not null
+{% endmacro %}
+
  {% macro qa_get_unique_count_base(model, column_name) %}
 
 
@@ -68,10 +88,9 @@ from (
 ) validation_errors
 {% endmacro %}
 
- {% macro qa_all_not_null_base(model, column_name) %}
+ {% macro qa_multi_models_base(model1, col_model_1, model2, model3, model4, model5) %}
 
-
-select * from {{ model }} where {{ column_name }} is not null
+select {{model1}}.{{col_model_1}} from {{model1}},{{model2}},{{model3}},{{model4}},{{model5}}
 {% endmacro %}
 
  {% macro qa_complex_macro_base(model, column_name_int, accepted_values=[1, 2]) %}
@@ -98,14 +117,4 @@ payments_validation_errors as (
 select * from payments_validation_errors
 {% endmacro %}
 
- {% macro qa_model_all_above_given_id_base(model, col, id_min=2) %}
-
-
-SELECT * from {{model}} where {{col}} > {{ id_min }}
-{% endmacro %}
-
- {% macro qa_all_null_base(model='customers', column_name='id') %}
-
-
-select * from {{ model }} where {{ column_name }} is not null
-{% endmacro %}
+ 
