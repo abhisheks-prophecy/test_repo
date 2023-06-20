d3b6ce2b089a0fd577cc3e48ab345b94496f7166
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2023-06-20 08:33:02 +0000

    ❇️ New     - Pipeline        - Workflow-Automated-ROLLBACK-1687249769134

---
+++ uitesting/e2etestsmainn8_external/pipelines/Workflow-Automated-ROLLBACK-1687249769134/code/src/main/scala/io/prophecy/pipelines/workflowautomatedrollback1687249769134/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomatedrollback1687249769134.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
