0cb61807402cee4da65e22d8eac1552845b91766
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2023-06-21 04:59:12 +0000

    ❇️ New     - Pipeline        - Automated-Github-UpdateToken-Pipeline

---
+++ hotfix/e2etestsmainn/updatetoken1/pipelines/Automated-Github-UpdateToken-Pipeline/code/src/main/scala/io/prophecy/pipelines/automatedgithubupdatetokenpipeline/main.scala
@@ -0,0 +1,45 @@
+package io.prophecy.pipelines.automatedgithubupdatetokenpipeline
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.automatedgithubupdatetokenpipeline.config.Context
+import io.prophecy.pipelines.automatedgithubupdatetokenpipeline.config._
+import io.prophecy.pipelines.automatedgithubupdatetokenpipeline.udfs.UDFs._
+import io.prophecy.pipelines.automatedgithubupdatetokenpipeline.udfs._
+import io.prophecy.pipelines.automatedgithubupdatetokenpipeline.udfs.PipelineInitCode._
+import io.prophecy.pipelines.automatedgithubupdatetokenpipeline.graph._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Main {
+
+  def apply(context: Context): Unit = {
+    val df_Script_0 = Script_0(context)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val config = ConfigurationFactoryImpl.getConfig(args)
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("Prophecy Pipeline")
+      .config("spark.default.parallelism",             "4")
+      .config("spark.sql.legacy.allowUntypedScalaUDF", "true")
+      .enableHiveSupport()
+      .getOrCreate()
+      .newSession()
+    val context = Context(spark, config)
+    spark.conf.set("prophecy.metadata.pipeline.uri",
+                   "pipelines/Automated-Github-UpdateToken-Pipeline"
+    )
+    registerUDFs(spark)
+    MetricsCollector.start(spark,
+                           "pipelines/Automated-Github-UpdateToken-Pipeline"
+    )
+    apply(context)
+    MetricsCollector.end(spark)
+  }
+
+}
