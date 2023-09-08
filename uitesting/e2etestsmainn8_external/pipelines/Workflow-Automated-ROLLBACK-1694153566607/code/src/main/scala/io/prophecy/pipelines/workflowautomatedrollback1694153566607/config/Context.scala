feb431ae2dd3a24922a7ae674ee7c7b4cecf55a2
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2023-09-08 06:14:50 +0000

    ❇️ New     - Pipeline        - Workflow-Automated-ROLLBACK-1694153566607

---
+++ uitesting/e2etestsmainn8_external/pipelines/Workflow-Automated-ROLLBACK-1694153566607/code/src/main/scala/io/prophecy/pipelines/workflowautomatedrollback1694153566607/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.workflowautomatedrollback1694153566607.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
---
+++ uitesting/e2etestsmainn8_external/pipelines/Workflow-Automated-ROLLBACK-1694153566607/code/src/main/scala/io/prophecy/pipelines/workflowautomatedrollback1694153566607/main.scala
@@ -0,0 +1,51 @@
+package io.prophecy.pipelines.workflowautomatedrollback1694153566607
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.workflowautomatedrollback1694153566607.config.Context
+import io.prophecy.pipelines.workflowautomatedrollback1694153566607.config._
+import io.prophecy.pipelines.workflowautomatedrollback1694153566607.udfs.UDFs._
+import io.prophecy.pipelines.workflowautomatedrollback1694153566607.udfs._
+import io.prophecy.pipelines.workflowautomatedrollback1694153566607.udfs.PipelineInitCode._
+import io.prophecy.pipelines.workflowautomatedrollback1694153566607.graph._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Main {
+  def apply(context: Context): Unit = {}
+
+  def main(args:     Array[String]): Unit = {
+    val config = ConfigurationFactoryImpl.getConfig(args)
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("Prophecy Pipeline")
+      .config("spark.default.parallelism",             "4")
+      .config("spark.sql.legacy.allowUntypedScalaUDF", "true")
+      .enableHiveSupport()
+      .getOrCreate()
+      .newSession()
+    val context = Context(spark, config)
+    spark.conf.set("prophecy.metadata.pipeline.uri",
+                   "pipelines/Workflow-Automated-ROLLBACK-1694153566607"
+    )
+    registerUDFs(spark)
+    try MetricsCollector.start(
+      spark,
+      "pipelines/Workflow-Automated-ROLLBACK-1694153566607",
+      context.config
+    )
+    catch {
+      case _: Throwable =>
+        MetricsCollector.start(
+          spark,
+          "pipelines/Workflow-Automated-ROLLBACK-1694153566607"
+        )
+    }
+    apply(context)
+    MetricsCollector.end(spark)
+  }
+
+}
