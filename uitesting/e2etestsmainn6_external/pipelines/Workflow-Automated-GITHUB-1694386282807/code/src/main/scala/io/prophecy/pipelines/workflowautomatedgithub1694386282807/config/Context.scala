6dca5984ccd2b5e40b454555850cbda2863b0219
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2023-09-10 22:53:50 +0000

    ❇️ New     - Pipeline        - Workflow-Automated-GITHUB-1694386282807

---
+++ uitesting/e2etestsmainn6_external/pipelines/Workflow-Automated-GITHUB-1694386282807/code/src/main/scala/io/prophecy/pipelines/workflowautomatedgithub1694386282807/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.workflowautomatedgithub1694386282807.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
---
+++ uitesting/e2etestsmainn6_external/pipelines/Workflow-Automated-GITHUB-1694386282807/code/src/main/scala/io/prophecy/pipelines/workflowautomatedgithub1694386282807/main.scala
@@ -0,0 +1,51 @@
+package io.prophecy.pipelines.workflowautomatedgithub1694386282807
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.workflowautomatedgithub1694386282807.config.Context
+import io.prophecy.pipelines.workflowautomatedgithub1694386282807.config._
+import io.prophecy.pipelines.workflowautomatedgithub1694386282807.udfs.UDFs._
+import io.prophecy.pipelines.workflowautomatedgithub1694386282807.udfs._
+import io.prophecy.pipelines.workflowautomatedgithub1694386282807.udfs.PipelineInitCode._
+import io.prophecy.pipelines.workflowautomatedgithub1694386282807.graph._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Main {
+  def apply(context: Context): Unit = {}
+
+  def main(args:     Array[String]): Unit = {
+    val config = ConfigurationFactoryImpl.getConfig(args)
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("Prophecy Pipeline")
+      .config("spark.default.parallelism",             "4")
+      .config("spark.sql.legacy.allowUntypedScalaUDF", "true")
+      .enableHiveSupport()
+      .getOrCreate()
+      .newSession()
+    val context = Context(spark, config)
+    spark.conf.set("prophecy.metadata.pipeline.uri",
+                   "pipelines/Workflow-Automated-GITHUB-1694386282807"
+    )
+    registerUDFs(spark)
+    try MetricsCollector.start(
+      spark,
+      "pipelines/Workflow-Automated-GITHUB-1694386282807",
+      context.config
+    )
+    catch {
+      case _: Throwable =>
+        MetricsCollector.start(
+          spark,
+          "pipelines/Workflow-Automated-GITHUB-1694386282807"
+        )
+    }
+    apply(context)
+    MetricsCollector.end(spark)
+  }
+
+}
