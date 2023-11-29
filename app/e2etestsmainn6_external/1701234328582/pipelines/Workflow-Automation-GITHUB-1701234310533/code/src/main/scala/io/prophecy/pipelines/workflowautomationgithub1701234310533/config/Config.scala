31e19e6be439a2d33bb4a6fbcb44dd42d9d8cef7
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2023-11-29 05:07:46 +0000

    ❇️ New     - Pipeline        - Workflow-Automation-GITHUB-1701234310533

---
+++ app/e2etestsmainn6_external/1701234328582/pipelines/Workflow-Automation-GITHUB-1701234310533/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1701234310533/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomationgithub1701234310533.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
