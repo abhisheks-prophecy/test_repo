b5d9d3e876736e10d6ebecabab5c2c40135cfe3e
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2023-12-03 06:08:10 +0000

    ❇️ New     - Pipeline        - Workflow-Automation-GITHUB-1701583546246

---
+++ staging/e2etestsmainn6_external/1701583563279/pipelines/Workflow-Automation-GITHUB-1701583546246/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1701583546246/main.scala
@@ -0,0 +1,51 @@
+package io.prophecy.pipelines.workflowautomationgithub1701583546246
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.workflowautomationgithub1701583546246.config.Context
+import io.prophecy.pipelines.workflowautomationgithub1701583546246.config._
+import io.prophecy.pipelines.workflowautomationgithub1701583546246.udfs.UDFs._
+import io.prophecy.pipelines.workflowautomationgithub1701583546246.udfs._
+import io.prophecy.pipelines.workflowautomationgithub1701583546246.udfs.PipelineInitCode._
+import io.prophecy.pipelines.workflowautomationgithub1701583546246.graph._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Main {
+  def apply(context: Context): Unit = {}
+
+  def main(args:     Array[String]): Unit = {
+    val config = ConfigurationFactoryImpl.getConfig(args)
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("Prophecy Pipeline")
+      .config("spark.default.parallelism",             "4")
+      .config("spark.sql.legacy.allowUntypedScalaUDF", "true")
+      .enableHiveSupport()
+      .getOrCreate()
+      .newSession()
+    val context = Context(spark, config)
+    spark.conf.set("prophecy.metadata.pipeline.uri",
+                   "pipelines/Workflow-Automation-GITHUB-1701583546246"
+    )
+    registerUDFs(spark)
+    try MetricsCollector.start(
+      spark,
+      "pipelines/Workflow-Automation-GITHUB-1701583546246",
+      context.config
+    )
+    catch {
+      case _: Throwable =>
+        MetricsCollector.start(
+          spark,
+          "pipelines/Workflow-Automation-GITHUB-1701583546246"
+        )
+    }
+    apply(context)
+    MetricsCollector.end(spark)
+  }
+
+}
