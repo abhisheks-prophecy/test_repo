79913a509a6d10b3f6f4d7cd74ebc9ed424243e6
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-12-06 15:43:26 +0000

    🔁 Modified     - Job        - SQLDbPub_Composer

--- app/dboub/maintest1/jobs/SQLDbPub_Composer/code/abhisheks_e2etests_sql_dbpublic_sqldbpub_composer/tasks/DBT_0.py
+++ app/dboub/maintest1/jobs/SQLDbPub_Composer/code/abhisheks_e2etests_sql_dbpublic_sqldbpub_composer/tasks/DBT_0.py
@@ -4,7 +4,7 @@ def DBT_0():
 
     return BashOperator(
         task_id = "DBT_0",
-        bash_command = "set -euxo pipefail; tmpDir=`mktemp -d`; git clone https://github.com/abhisheks-prophecy/sql_db_app --branch dev/Abhishek1 --single-branch $tmpDir; cd $tmpDir/; dbt deps --profile run_profile; dbt seed --profile run_profile; dbt run --profile run_profile; dbt test --profile run_profile; ",
+        bash_command = "set -euxo pipefail; tmpDir=`mktemp -d`; git clone https://github.com/abhisheks-prophecy/sql_db_app --branch main --single-branch $tmpDir; cd $tmpDir/; dbt deps --profile run_profile; dbt seed --profile run_profile; dbt run --profile run_profile; dbt test --profile run_profile; ",
         env = {"DBT_PROFILES_DIR" : "/home/airflow/gcs/data", "DBT_FULL_REFRESH" : "true"},
         append_env = True,
     )
