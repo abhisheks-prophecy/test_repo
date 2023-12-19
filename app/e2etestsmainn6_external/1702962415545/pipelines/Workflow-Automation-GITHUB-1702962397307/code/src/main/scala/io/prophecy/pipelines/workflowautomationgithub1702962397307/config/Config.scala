837acc4eb162199df5f57f67157064d8ab7d5e19
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2023-12-19 05:08:54 +0000

    ❇️ New     - Pipeline        - Workflow-Automation-GITHUB-1702962397307

---
+++ app/e2etestsmainn6_external/1702962415545/pipelines/Workflow-Automation-GITHUB-1702962397307/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1702962397307/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomationgithub1702962397307.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
---
+++ app/e2etestsmainn6_external/1702962415545/pipelines/Workflow-Automation-GITHUB-1702962397307/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1702962397307/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.workflowautomationgithub1702962397307.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
