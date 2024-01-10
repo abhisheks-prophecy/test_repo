cfc78b9a090a5c806e48a87610420eb0610ed844
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2024-01-10 14:22:36 +0000

    ❇️ New     - Pipeline        - Workflow-Automation-ROLLBACK-1704896470887

---
+++ staging/e2etestsmainn8_external/1704896488063/pipelines/Workflow-Automation-ROLLBACK-1704896470887/code/src/main/scala/io/prophecy/pipelines/workflowautomationrollback1704896470887/main.scala
@@ -0,0 +1,51 @@
+package io.prophecy.pipelines.workflowautomationrollback1704896470887
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.workflowautomationrollback1704896470887.config.Context
+import io.prophecy.pipelines.workflowautomationrollback1704896470887.config._
+import io.prophecy.pipelines.workflowautomationrollback1704896470887.udfs.UDFs._
+import io.prophecy.pipelines.workflowautomationrollback1704896470887.udfs._
+import io.prophecy.pipelines.workflowautomationrollback1704896470887.udfs.PipelineInitCode._
+import io.prophecy.pipelines.workflowautomationrollback1704896470887.graph._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Main {
+  def apply(context: Context): Unit = {}
+
+  def main(args:     Array[String]): Unit = {
+    val config = ConfigurationFactoryImpl.getConfig(args)
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("Prophecy Pipeline")
+      .config("spark.default.parallelism",             "4")
+      .config("spark.sql.legacy.allowUntypedScalaUDF", "true")
+      .enableHiveSupport()
+      .getOrCreate()
+      .newSession()
+    val context = Context(spark, config)
+    spark.conf.set("prophecy.metadata.pipeline.uri",
+                   "pipelines/Workflow-Automation-ROLLBACK-1704896470887"
+    )
+    registerUDFs(spark)
+    try MetricsCollector.start(
+      spark,
+      "pipelines/Workflow-Automation-ROLLBACK-1704896470887",
+      context.config
+    )
+    catch {
+      case _: Throwable =>
+        MetricsCollector.start(
+          spark,
+          "pipelines/Workflow-Automation-ROLLBACK-1704896470887"
+        )
+    }
+    apply(context)
+    MetricsCollector.end(spark)
+  }
+
+}
