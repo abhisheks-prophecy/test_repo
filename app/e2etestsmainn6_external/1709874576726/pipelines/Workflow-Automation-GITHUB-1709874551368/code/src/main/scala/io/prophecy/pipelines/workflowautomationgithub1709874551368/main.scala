0a442103446ab396044e03a0dbb6ba903fd37189
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2024-03-08 05:12:19 +0000

    ❇️ New     - Pipeline        - Workflow-Automation-GITHUB-1709874551368

---
+++ app/e2etestsmainn6_external/1709874576726/pipelines/Workflow-Automation-GITHUB-1709874551368/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1709874551368/main.scala
@@ -0,0 +1,49 @@
+package io.prophecy.pipelines.workflowautomationgithub1709874551368
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.workflowautomationgithub1709874551368.config._
+import io.prophecy.pipelines.workflowautomationgithub1709874551368.udfs.UDFs._
+import io.prophecy.pipelines.workflowautomationgithub1709874551368.udfs.PipelineInitCode._
+import io.prophecy.pipelines.workflowautomationgithub1709874551368.graph._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Main {
+  def apply(context: Context): Unit = {}
+
+  def main(args:     Array[String]): Unit = {
+    val config = ConfigurationFactoryImpl.getConfig(args)
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("Prophecy Pipeline")
+      .config("spark.default.parallelism",             "4")
+      .config("spark.sql.legacy.allowUntypedScalaUDF", "true")
+      .enableHiveSupport()
+      .getOrCreate()
+      .newSession()
+    val context = Context(spark, config)
+    spark.conf.set("prophecy.metadata.pipeline.uri",
+                   "pipelines/Workflow-Automation-GITHUB-1709874551368"
+    )
+    registerUDFs(spark)
+    try MetricsCollector.start(
+      spark,
+      "pipelines/Workflow-Automation-GITHUB-1709874551368",
+      context.config
+    )
+    catch {
+      case _: Throwable =>
+        MetricsCollector.start(
+          spark,
+          "pipelines/Workflow-Automation-GITHUB-1709874551368"
+        )
+    }
+    apply(context)
+    MetricsCollector.end(spark)
+  }
+
+}
