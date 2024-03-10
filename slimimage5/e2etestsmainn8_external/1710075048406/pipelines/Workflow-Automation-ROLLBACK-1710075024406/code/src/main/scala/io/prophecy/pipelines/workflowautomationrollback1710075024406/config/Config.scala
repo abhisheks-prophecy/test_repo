bc295fd3682138bf58f3d33715d01105ee4dfb2a
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2024-03-10 12:53:29 +0000

    ❇️ New     - Pipeline        - Workflow-Automation-ROLLBACK-1710075024406

---
+++ slimimage5/e2etestsmainn8_external/1710075048406/pipelines/Workflow-Automation-ROLLBACK-1710075024406/code/src/main/scala/io/prophecy/pipelines/workflowautomationrollback1710075024406/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomationrollback1710075024406.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
---
+++ slimimage5/e2etestsmainn8_external/1710075048406/pipelines/Workflow-Automation-ROLLBACK-1710075024406/code/src/main/scala/io/prophecy/pipelines/workflowautomationrollback1710075024406/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.workflowautomationrollback1710075024406.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
