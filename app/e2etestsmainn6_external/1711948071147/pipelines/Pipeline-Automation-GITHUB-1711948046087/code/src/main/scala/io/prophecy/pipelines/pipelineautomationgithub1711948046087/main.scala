c6a1e61db302a7f4d708192d92429e680079d96a
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2024-04-01 05:19:21 +0000

    prophecy test commit

---
+++ app/e2etestsmainn6_external/1711948071147/pipelines/Pipeline-Automation-GITHUB-1711948046087/code/src/main/scala/io/prophecy/pipelines/pipelineautomationgithub1711948046087/main.scala
@@ -0,0 +1,49 @@
+package io.prophecy.pipelines.pipelineautomationgithub1711948046087
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.pipelineautomationgithub1711948046087.config._
+import io.prophecy.pipelines.pipelineautomationgithub1711948046087.udfs.UDFs._
+import io.prophecy.pipelines.pipelineautomationgithub1711948046087.udfs.PipelineInitCode._
+import io.prophecy.pipelines.pipelineautomationgithub1711948046087.graph._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Main {
+  def apply(context: Context): Unit = {}
+
+  def main(args:     Array[String]): Unit = {
+    val config = ConfigurationFactoryImpl.getConfig(args)
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("Prophecy Pipeline")
+      .config("spark.default.parallelism",             "4")
+      .config("spark.sql.legacy.allowUntypedScalaUDF", "true")
+      .enableHiveSupport()
+      .getOrCreate()
+      .newSession()
+    val context = Context(spark, config)
+    spark.conf.set("prophecy.metadata.pipeline.uri",
+                   "pipelines/Pipeline-Automation-GITHUB-1711948046087"
+    )
+    registerUDFs(spark)
+    try MetricsCollector.start(
+      spark,
+      "pipelines/Pipeline-Automation-GITHUB-1711948046087",
+      context.config
+    )
+    catch {
+      case _: Throwable =>
+        MetricsCollector.start(
+          spark,
+          "pipelines/Pipeline-Automation-GITHUB-1711948046087"
+        )
+    }
+    apply(context)
+    MetricsCollector.end(spark)
+  }
+
+}
