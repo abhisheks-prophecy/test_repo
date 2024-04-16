0730e6795816aa9cb5ce4a4818c83dcfc03ceeb9
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2024-04-16 21:31:37 +0000

    ❇️ New     - Pipeline        - Automation-Github-UpdateToken-Pipeline

---
+++ perftest/e2etestsmainn/updatetoken1/1713302856068/pipelines/Automation-Github-UpdateToken-Pipeline/code/src/main/scala/io/prophecy/pipelines/automationgithubupdatetokenpipeline/graph/Script_0.scala
@@ -0,0 +1,22 @@
+package io.prophecy.pipelines.automationgithubupdatetokenpipeline.graph
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.automationgithubupdatetokenpipeline.config.Context
+import io.prophecy.pipelines.automationgithubupdatetokenpipeline.udfs.UDFs._
+import io.prophecy.pipelines.automationgithubupdatetokenpipeline.udfs.PipelineInitCode._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Script_0 {
+  def apply(context: Context, in0: DataFrame): DataFrame = {
+    val spark = context.spark
+    val Config = context.config
+    
+    out0
+  }
+
+}
