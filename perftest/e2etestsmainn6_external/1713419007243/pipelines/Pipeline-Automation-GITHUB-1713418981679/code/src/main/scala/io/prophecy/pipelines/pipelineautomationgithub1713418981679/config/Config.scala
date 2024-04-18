627ba58f01b90b34ab56ad64d95a8f43bcaa9c50
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2024-04-18 05:55:36 +0000

    prophecy test commit

---
+++ perftest/e2etestsmainn6_external/1713419007243/pipelines/Pipeline-Automation-GITHUB-1713418981679/code/src/main/scala/io/prophecy/pipelines/pipelineautomationgithub1713418981679/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.pipelineautomationgithub1713418981679.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
