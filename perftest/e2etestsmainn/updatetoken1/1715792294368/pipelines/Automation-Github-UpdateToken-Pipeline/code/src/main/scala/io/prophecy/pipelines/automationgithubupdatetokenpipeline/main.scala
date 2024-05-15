c5c1f476ae4347c4640737f4adb30ee0682acd10
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2024-05-15 17:01:32 +0000

    ❇️ New     - Pipeline        - Automation-Github-UpdateToken-Pipeline

---
+++ perftest/e2etestsmainn/updatetoken1/1715792294368/pipelines/Automation-Github-UpdateToken-Pipeline/code/src/main/scala/io/prophecy/pipelines/automationgithubupdatetokenpipeline/main.scala
@@ -0,0 +1,43 @@
+package io.prophecy.pipelines.automationgithubupdatetokenpipeline
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.automationgithubupdatetokenpipeline.config._
+import io.prophecy.pipelines.automationgithubupdatetokenpipeline.udfs.UDFs._
+import io.prophecy.pipelines.automationgithubupdatetokenpipeline.udfs.PipelineInitCode._
+import io.prophecy.pipelines.automationgithubupdatetokenpipeline.graph._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Main {
+
+  def apply(context: Context): Unit = {
+    val df_Script_0 = Script_0(context)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val config = ConfigurationFactoryImpl.getConfig(args)
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("Automation-Github-UpdateToken-Pipeline")
+      .config("spark.default.parallelism",             "4")
+      .config("spark.sql.legacy.allowUntypedScalaUDF", "true")
+      .enableHiveSupport()
+      .getOrCreate()
+    val context = Context(spark, config)
+    spark.conf.set("prophecy.metadata.pipeline.uri",
+                   "pipelines/Automation-Github-UpdateToken-Pipeline"
+    )
+    registerUDFs(spark)
+    MetricsCollector.instrument(
+      spark,
+      "pipelines/Automation-Github-UpdateToken-Pipeline"
+    ) {
+      apply(context)
+    }
+  }
+
+}
