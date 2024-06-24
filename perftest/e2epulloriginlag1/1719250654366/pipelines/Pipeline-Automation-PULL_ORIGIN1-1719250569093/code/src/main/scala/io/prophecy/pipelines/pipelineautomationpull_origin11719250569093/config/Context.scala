34a745a710adc0d9b15b9571bfbcd3a2dbf588c0
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2024-06-24 17:41:33 +0000

    Adds new pipeline 'Pipeline-Automation-PULL_ORIGIN1-1719250569093' with configuration and resources

---
+++ perftest/e2epulloriginlag1/1719250654366/pipelines/Pipeline-Automation-PULL_ORIGIN1-1719250569093/code/src/main/scala/io/prophecy/pipelines/pipelineautomationpull_origin11719250569093/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.pipelineautomationpull_origin11719250569093.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
---
+++ perftest/e2epulloriginlag1/1719250654366/pipelines/Pipeline-Automation-PULL_ORIGIN1-1719250569093/code/src/main/scala/io/prophecy/pipelines/pipelineautomationpull_origin11719250569093/main.scala
@@ -0,0 +1,43 @@
+package io.prophecy.pipelines.pipelineautomationpull_origin11719250569093
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.pipelineautomationpull_origin11719250569093.config._
+import io.prophecy.pipelines.pipelineautomationpull_origin11719250569093.functions.UDFs._
+import io.prophecy.pipelines.pipelineautomationpull_origin11719250569093.functions.PipelineInitCode._
+import io.prophecy.pipelines.pipelineautomationpull_origin11719250569093.graph._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Main {
+
+  def apply(context: Context): Unit = {
+    val df_Script_0 = Script_0(context)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val config = ConfigurationFactoryImpl.getConfig(args)
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("Pipeline-Automation-PULL_ORIGIN1-1719250569093")
+      .config("spark.default.parallelism",             "4")
+      .config("spark.sql.legacy.allowUntypedScalaUDF", "true")
+      .enableHiveSupport()
+      .getOrCreate()
+    val context = Context(spark, config)
+    spark.conf.set("prophecy.metadata.pipeline.uri",
+                   "pipelines/Pipeline-Automation-PULL_ORIGIN1-1719250569093"
+    )
+    registerUDFs(spark)
+    MetricsCollector.instrument(
+      spark,
+      "pipelines/Pipeline-Automation-PULL_ORIGIN1-1719250569093"
+    ) {
+      apply(context)
+    }
+  }
+
+}
