796b653861d2f0822920afa5ca6d984368032a88
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2024-06-27 00:48:17 +0000

    Adds new pipeline 'Workflow-Automation-GITHUB-1719449098465' with configuration and Scala files

---
+++ test/e2etestsmainn6_external/1719449116073/pipelines/Workflow-Automation-GITHUB-1719449098465/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1719449098465/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomationgithub1719449098465.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
---
+++ test/e2etestsmainn6_external/1719449116073/pipelines/Workflow-Automation-GITHUB-1719449098465/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1719449098465/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.workflowautomationgithub1719449098465.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
