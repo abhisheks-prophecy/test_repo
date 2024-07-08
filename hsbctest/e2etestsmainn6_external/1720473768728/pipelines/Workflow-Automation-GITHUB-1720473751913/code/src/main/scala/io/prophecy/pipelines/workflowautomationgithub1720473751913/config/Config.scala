d42fe963e18d825ae002e540a9b481cd644d669f
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2024-07-08 21:25:48 +0000

    ❇️ New     - Pipeline        - Workflow-Automation-GITHUB-1720473751913

---
+++ hsbctest/e2etestsmainn6_external/1720473768728/pipelines/Workflow-Automation-GITHUB-1720473751913/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1720473751913/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomationgithub1720473751913.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
