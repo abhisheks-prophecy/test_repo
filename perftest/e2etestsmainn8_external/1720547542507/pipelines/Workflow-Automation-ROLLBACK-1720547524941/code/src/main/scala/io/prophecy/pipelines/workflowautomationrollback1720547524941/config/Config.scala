f0ca125f46f1133653c906cfff4363807b20c5a9
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2024-07-09 17:54:12 +0000

    Adds new pipeline 'Workflow-Automation-ROLLBACK-1720547524941' with associated configuration and function files

---
+++ perftest/e2etestsmainn8_external/1720547542507/pipelines/Workflow-Automation-ROLLBACK-1720547524941/code/src/main/scala/io/prophecy/pipelines/workflowautomationrollback1720547524941/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomationrollback1720547524941.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
---
+++ perftest/e2etestsmainn8_external/1720547542507/pipelines/Workflow-Automation-ROLLBACK-1720547524941/code/src/main/scala/io/prophecy/pipelines/workflowautomationrollback1720547524941/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.workflowautomationrollback1720547524941.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
