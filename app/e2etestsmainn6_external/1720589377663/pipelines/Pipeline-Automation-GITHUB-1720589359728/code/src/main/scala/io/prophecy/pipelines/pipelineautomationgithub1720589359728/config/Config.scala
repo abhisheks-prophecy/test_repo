23b70c4d05d4a452f13bac0a31fb82946e75fc64
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2024-07-10 05:38:38 +0000

    Ignore auto commit failure generation

---
+++ app/e2etestsmainn6_external/1720589377663/pipelines/Pipeline-Automation-GITHUB-1720589359728/code/src/main/scala/io/prophecy/pipelines/pipelineautomationgithub1720589359728/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.pipelineautomationgithub1720589359728.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
