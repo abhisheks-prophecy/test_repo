0dc4a56d69e499e530cd31297e1e4c6d288d3d9b
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2024-08-13 11:45:29 +0000

    ❇️ New     - Pipeline        - Workflow-Automation-ROLLBACK-1723549395781

---
+++ performance/e2etestsmainn8_external/1723549415142/pipelines/Workflow-Automation-ROLLBACK-1723549395781/code/src/main/scala/io/prophecy/pipelines/workflowautomationrollback1723549395781/functions/udafs.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomationrollback1723549395781.functions
+
+import org.apache.spark.sql.expressions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+object UDAFs { def registerUDAFs(spark: SparkSession): Unit = {} }
