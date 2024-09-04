661e83bb39dbe8c704752fdd9c71db5966e2d4d7
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2024-09-04 05:41:18 +0000

    prophecy test commit

---
+++ app/e2etestsmainn6_external/1725427811381/pipelines/Pipeline-Automation-GITHUB-1725427791192/code/src/main/scala/io/prophecy/pipelines/pipelineautomationgithub1725427791192/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.pipelineautomationgithub1725427791192.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
---
+++ app/e2etestsmainn6_external/1725427811381/pipelines/Pipeline-Automation-GITHUB-1725427791192/code/src/main/scala/io/prophecy/pipelines/pipelineautomationgithub1725427791192/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.pipelineautomationgithub1725427791192.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
