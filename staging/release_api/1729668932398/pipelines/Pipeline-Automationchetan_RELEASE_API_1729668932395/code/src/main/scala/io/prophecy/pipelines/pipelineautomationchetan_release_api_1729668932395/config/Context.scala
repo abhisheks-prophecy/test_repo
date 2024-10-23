845c4297c739505ef3d9f5d5bfc23e132351eaff
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2024-10-23 07:42:03 +0000

    Add new pipeline, dataset, job, and gem for Project-Automationchetan_RELEASE_API

---
+++ staging/release_api/1729668932398/pipelines/Pipeline-Automationchetan_RELEASE_API_1729668932395/code/src/main/scala/io/prophecy/pipelines/pipelineautomationchetan_release_api_1729668932395/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.pipelineautomationchetan_release_api_1729668932395.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
---
+++ staging/release_api/1729668932398/pipelines/Pipeline-Automationchetan_RELEASE_API_1729668932395/code/src/main/scala/io/prophecy/pipelines/pipelineautomationchetan_release_api_1729668932395/functions/udafs.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.pipelineautomationchetan_release_api_1729668932395.functions
+
+import org.apache.spark.sql.expressions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+object UDAFs { def registerUDAFs(spark: SparkSession): Unit = {} }
