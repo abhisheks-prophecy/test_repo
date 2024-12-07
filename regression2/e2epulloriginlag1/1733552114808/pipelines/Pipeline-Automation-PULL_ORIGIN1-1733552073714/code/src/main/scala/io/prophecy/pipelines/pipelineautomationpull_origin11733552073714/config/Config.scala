d35e33bf839702d113458063a25e979ed05e12be
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2024-12-07 06:18:15 +0000

    Add new pipeline 'Pipeline-Automation-PULL_ORIGIN1-1733552073714' with initial setup and configuration files

---
+++ regression2/e2epulloriginlag1/1733552114808/pipelines/Pipeline-Automation-PULL_ORIGIN1-1733552073714/code/src/main/scala/io/prophecy/pipelines/pipelineautomationpull_origin11733552073714/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.pipelineautomationpull_origin11733552073714.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
---
+++ regression2/e2epulloriginlag1/1733552114808/pipelines/Pipeline-Automation-PULL_ORIGIN1-1733552073714/code/src/main/scala/io/prophecy/pipelines/pipelineautomationpull_origin11733552073714/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.pipelineautomationpull_origin11733552073714.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
