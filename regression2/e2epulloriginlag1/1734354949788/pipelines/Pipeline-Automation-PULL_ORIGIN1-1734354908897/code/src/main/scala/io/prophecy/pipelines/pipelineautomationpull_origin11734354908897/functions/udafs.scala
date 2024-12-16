1b07952c04c3ae4bf46293f8e9409bf990069b9a
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2024-12-16 13:19:13 +0000

    Add new pipeline 'Pipeline-Automation-PULL_ORIGIN1-1734354908897' with initial configuration and setup files

---
+++ regression2/e2epulloriginlag1/1734354949788/pipelines/Pipeline-Automation-PULL_ORIGIN1-1734354908897/code/src/main/scala/io/prophecy/pipelines/pipelineautomationpull_origin11734354908897/functions/udafs.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.pipelineautomationpull_origin11734354908897.functions
+
+import org.apache.spark.sql.expressions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+object UDAFs { def registerUDAFs(spark: SparkSession): Unit = {} }
