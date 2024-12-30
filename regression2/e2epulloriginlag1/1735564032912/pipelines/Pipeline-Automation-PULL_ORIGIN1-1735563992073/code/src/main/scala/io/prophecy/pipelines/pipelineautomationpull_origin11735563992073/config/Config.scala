0e59d8e1c6857164ed874880b4ffd6576ad95436
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2024-12-30 13:10:37 +0000

    Add new pipeline 'Pipeline-Automation-PULL_ORIGIN1-1735563992073' with initial setup and configuration files.

---
+++ regression2/e2epulloriginlag1/1735564032912/pipelines/Pipeline-Automation-PULL_ORIGIN1-1735563992073/code/src/main/scala/io/prophecy/pipelines/pipelineautomationpull_origin11735563992073/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.pipelineautomationpull_origin11735563992073.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
