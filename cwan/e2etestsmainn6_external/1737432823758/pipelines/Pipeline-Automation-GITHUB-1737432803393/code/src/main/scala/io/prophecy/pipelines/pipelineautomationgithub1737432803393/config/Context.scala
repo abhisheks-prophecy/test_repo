473ed9ebc47c51cd3ceda1d67e9a16bb6cab8c79
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2025-01-21 04:26:51 +0000

    prophecy test commit

---
+++ cwan/e2etestsmainn6_external/1737432823758/pipelines/Pipeline-Automation-GITHUB-1737432803393/code/src/main/scala/io/prophecy/pipelines/pipelineautomationgithub1737432803393/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.pipelineautomationgithub1737432803393.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
---
+++ cwan/e2etestsmainn6_external/1737432823758/pipelines/Pipeline-Automation-GITHUB-1737432803393/code/src/main/scala/io/prophecy/pipelines/pipelineautomationgithub1737432803393/functions/udafs.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.pipelineautomationgithub1737432803393.functions
+
+import org.apache.spark.sql.expressions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+object UDAFs { def registerUDAFs(spark: SparkSession): Unit = {} }
