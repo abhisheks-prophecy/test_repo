4e8a5c5b9e80a4811b4bdc6728f69805b2de384b
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2025-03-21 04:25:17 +0000

    prophecy test commit

---
+++ cwan/e2etestsmainn6_external/1742530265938/pipelines/Pipeline-Automation-GITHUB-1742530245366/code/src/main/scala/io/prophecy/pipelines/pipelineautomationgithub1742530245366/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.pipelineautomationgithub1742530245366.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
---
+++ cwan/e2etestsmainn6_external/1742530265938/pipelines/Pipeline-Automation-GITHUB-1742530245366/code/src/main/scala/io/prophecy/pipelines/pipelineautomationgithub1742530245366/main.scala
@@ -0,0 +1,40 @@
+package io.prophecy.pipelines.pipelineautomationgithub1742530245366
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.pipelineautomationgithub1742530245366.config._
+import io.prophecy.pipelines.pipelineautomationgithub1742530245366.functions.UDFs._
+import io.prophecy.pipelines.pipelineautomationgithub1742530245366.functions.PipelineInitCode._
+import io.prophecy.pipelines.pipelineautomationgithub1742530245366.graph._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Main {
+  def apply(context: Context): Unit = {}
+
+  def main(args:     Array[String]): Unit = {
+    val config = ConfigurationFactoryImpl.getConfig(args)
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("Pipeline-Automation-GITHUB-1742530245366")
+      .enableHiveSupport()
+      .getOrCreate()
+    val context = Context(spark, config)
+    spark.conf.set("prophecy.metadata.pipeline.uri",
+                   "pipelines/Pipeline-Automation-GITHUB-1742530245366"
+    )
+    spark.conf.set("spark.default.parallelism",             "4")
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
+    registerUDFs(spark)
+    MetricsCollector.instrument(
+      spark,
+      "pipelines/Pipeline-Automation-GITHUB-1742530245366"
+    ) {
+      apply(context)
+    }
+  }
+
+}
