9bb68e9a27c0c1468423485dc745a2df1b23b4e8
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2025-04-07 10:28:31 +0000

    Add new pipeline Workflow-Automation-ROLLBACK-1744021554499 with configuration and dependencies

---
+++ regression2/e2etestsmainn8_external/1744021578496/pipelines/Workflow-Automation-ROLLBACK-1744021554499/code/src/main/scala/io/prophecy/pipelines/workflowautomationrollback1744021554499/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.workflowautomationrollback1744021554499.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
---
+++ regression2/e2etestsmainn8_external/1744021578496/pipelines/Workflow-Automation-ROLLBACK-1744021554499/code/src/main/scala/io/prophecy/pipelines/workflowautomationrollback1744021554499/main.scala
@@ -0,0 +1,40 @@
+package io.prophecy.pipelines.workflowautomationrollback1744021554499
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.workflowautomationrollback1744021554499.config._
+import io.prophecy.pipelines.workflowautomationrollback1744021554499.functions.UDFs._
+import io.prophecy.pipelines.workflowautomationrollback1744021554499.functions.PipelineInitCode._
+import io.prophecy.pipelines.workflowautomationrollback1744021554499.graph._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Main {
+  def apply(context: Context): Unit = {}
+
+  def main(args:     Array[String]): Unit = {
+    val config = ConfigurationFactoryImpl.getConfig(args)
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("Workflow-Automation-ROLLBACK-1744021554499")
+      .enableHiveSupport()
+      .getOrCreate()
+    val context = Context(spark, config)
+    spark.conf.set("prophecy.metadata.pipeline.uri",
+                   "pipelines/Workflow-Automation-ROLLBACK-1744021554499"
+    )
+    spark.conf.set("spark.default.parallelism",             "4")
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
+    registerUDFs(spark)
+    MetricsCollector.instrument(
+      spark,
+      "pipelines/Workflow-Automation-ROLLBACK-1744021554499"
+    ) {
+      apply(context)
+    }
+  }
+
+}
