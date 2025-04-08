e01a4c88e5a46acf601bd4fea12dce4d596f3aa3
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2025-04-08 10:03:18 +0000

    Reverted to 'This is test commit message1' ,commit hash : d678cbe0050290f7575202925a1b5244fc9230d8

--- test/can_delete1744106132571/rollBackProjects/pipelines/Automation_RollbackChanges_Pipeline2/code/src/main/scala/io/prophecy/pipelines/automation_rollbackchanges_pipeline2/functions/udafs.scala
+++
@@ -1,6 +0,0 @@
-package io.prophecy.pipelines.automation_rollbackchanges_pipeline2.functions
-
-import org.apache.spark.sql.expressions._
-import org.apache.spark.sql.types._
-import org.apache.spark.sql._
-object UDAFs { def registerUDAFs(spark: SparkSession): Unit = {} }
