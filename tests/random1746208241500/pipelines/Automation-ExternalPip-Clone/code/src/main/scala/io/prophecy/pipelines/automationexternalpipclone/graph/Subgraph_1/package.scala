8dd27ef9dfd86824cb2a66f95f5f6744e570b16e
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2025-05-02 17:51:48 +0000

    Cloning prophecy project main branch

---
+++ tests/random1746208241500/pipelines/Automation-ExternalPip-Clone/code/src/main/scala/io/prophecy/pipelines/automationexternalpipclone/graph/Subgraph_1/package.scala
@@ -0,0 +1,20 @@
+package io.prophecy.pipelines.automationexternalpipclone.graph
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.automationexternalpipclone.functions.PipelineInitCode._
+import io.prophecy.pipelines.automationexternalpipclone.graph.Subgraph_1.config._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+package object Subgraph_1 {
+
+  def apply(context: Context, in0: DataFrame): DataFrame = {
+    val df_customer_details_projection =
+      customer_details_projection(context, in0)
+    df_customer_details_projection
+  }
+
+}
