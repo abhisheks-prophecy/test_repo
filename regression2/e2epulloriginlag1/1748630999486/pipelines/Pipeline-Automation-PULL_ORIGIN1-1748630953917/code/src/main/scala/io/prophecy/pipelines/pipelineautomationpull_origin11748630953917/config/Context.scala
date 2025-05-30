a764003c468bdeba53344faa29caa11216b58544
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2025-05-30 18:53:52 +0000

    Add new pipeline 'Pipeline-Automation-PULL_ORIGIN1' with initial setup and configuration files.

---
+++ regression2/e2epulloriginlag1/1748630999486/pipelines/Pipeline-Automation-PULL_ORIGIN1-1748630953917/code/src/main/scala/io/prophecy/pipelines/pipelineautomationpull_origin11748630953917/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.pipelineautomationpull_origin11748630953917.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
---
+++ regression2/e2epulloriginlag1/1748630999486/pipelines/Pipeline-Automation-PULL_ORIGIN1-1748630953917/code/src/main/scala/io/prophecy/pipelines/pipelineautomationpull_origin11748630953917/graph/Script_0.scala
@@ -0,0 +1,22 @@
+package io.prophecy.pipelines.pipelineautomationpull_origin11748630953917.graph
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.pipelineautomationpull_origin11748630953917.config.Context
+import io.prophecy.pipelines.pipelineautomationpull_origin11748630953917.functions.UDFs._
+import io.prophecy.pipelines.pipelineautomationpull_origin11748630953917.functions.PipelineInitCode._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Script_0 {
+  def apply(context: Context, in0: DataFrame): DataFrame = {
+    val spark = context.spark
+    val Config = context.config
+    
+    out0
+  }
+
+}
