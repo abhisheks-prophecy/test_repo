c7f60d13b867fbe0e05d44510bcb22e5c22e0bc7
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2025-05-30 18:56:23 +0000

    Update groupId in pom.xml and add Reformat_1 transformation to pipeline

---
+++ regression2/e2epulloriginlag1/1748630999486/pipelines/Pipeline-Automation-PULL_ORIGIN1-1748630953917/code/src/main/scala/io/prophecy/pipelines/pipelineautomationpull_origin11748630953917/graph/Reformat_1.scala
@@ -0,0 +1,13 @@
+package io.prophecy.pipelines.pipelineautomationpull_origin11748630953917.graph
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.pipelineautomationpull_origin11748630953917.functions.PipelineInitCode._
+import io.prophecy.pipelines.pipelineautomationpull_origin11748630953917.functions.UDFs._
+import io.prophecy.pipelines.pipelineautomationpull_origin11748630953917.config.Context
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+object Reformat_1 { def apply(context: Context, in: DataFrame): DataFrame = in }
