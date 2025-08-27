99b99db6c5c6590e22544ea9521e674fbb7e7e63
abhisheks-prophecy <98301588+abhisheks-prophecy@users.noreply.github.com>
2025-08-27 11:59:39 +0530

    Delete regression1 directory

--- regression1/e2etestsmainn6_external/1727107926340/pipelines/Pipeline-Automation-GITHUB-1727107909619/code/src/main/scala/io/prophecy/pipelines/pipelineautomationgithub1727107909619/config/Config.scala
+++
@@ -1,6 +0,0 @@
-package io.prophecy.pipelines.pipelineautomationgithub1727107909619.config
-
-import pureconfig._
-import pureconfig.generic.ProductHint
-import io.prophecy.libs._
-case class Config() extends ConfigBase
--- regression1/e2etestsmainn6_external/1727107926340/pipelines/Pipeline-Automation-GITHUB-1727107909619/code/src/main/scala/io/prophecy/pipelines/pipelineautomationgithub1727107909619/config/Context.scala
+++
@@ -1,4 +0,0 @@
-package io.prophecy.pipelines.pipelineautomationgithub1727107909619.config
-
-import org.apache.spark.sql.SparkSession
-case class Context(spark: SparkSession, config: Config)
