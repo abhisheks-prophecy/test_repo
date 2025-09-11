c75f38d6ae718470496fe8daae0eb2c629de2862
abhisheks-prophecy <98301588+abhisheks-prophecy@users.noreply.github.com>
2025-09-11 17:43:21 +0530

    Delete tests directory

--- tests/random1669291611328/pipelines/Automated-ExternalPip-Clone/code/src/main/scala/io/prophecy/pipelines/automatedexternalpipclone/config/Config.scala
+++
@@ -1,6 +0,0 @@
-package io.prophecy.pipelines.automatedexternalpipclone.config
-
-import io.prophecy.pipelines.automatedexternalpipclone.config.ConfigStore._
-import pureconfig._
-import io.prophecy.libs._
-case class Config() extends ConfigBase
--- tests/random1669303951254/README.md
+++
@@ -1,5 +0,0 @@
-# Automated-ExternalP-Clone Repository
-
-*** Release notes for version: 1.1 ***
-
-final release
