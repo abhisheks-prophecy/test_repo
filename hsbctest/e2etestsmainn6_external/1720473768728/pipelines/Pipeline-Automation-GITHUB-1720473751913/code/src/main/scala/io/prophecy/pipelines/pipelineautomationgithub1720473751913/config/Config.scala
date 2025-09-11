55ed1890d0b30f414592dce846e1fa9af08e77fe
abhisheks-prophecy <98301588+abhisheks-prophecy@users.noreply.github.com>
2025-09-11 17:40:27 +0530

    Delete hsbctest/e2etestsmainn6_external/1720473768728 directory

--- hsbctest/e2etestsmainn6_external/1720473768728/pipelines/Pipeline-Automation-GITHUB-1720473751913/code/src/main/scala/io/prophecy/pipelines/pipelineautomationgithub1720473751913/config/Config.scala
+++
@@ -1,6 +0,0 @@
-package io.prophecy.pipelines.pipelineautomationgithub1720473751913.config
-
-import pureconfig._
-import pureconfig.generic.ProductHint
-import io.prophecy.libs._
-case class Config() extends ConfigBase
