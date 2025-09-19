fb14a431a8654909a80448648ba230d4383b55af
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2025-09-19 06:59:52 +0000

    Simple workflow Squash and Rebase

---
+++ app/e2etestsmainn6_external/1757997348921/pipelines/Workflow-Automation-GITHUB-1757997337596/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1757997337596/functions/udafs.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomationgithub1757997337596.functions
+
+import org.apache.spark.sql.expressions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+object UDAFs { def registerUDAFs(spark: SparkSession): Unit = {} }
--- uitesting/e2etestsmainn6_external/1727782829379/pipelines/Pipeline-Automation-GITHUB-1727782809869/code/src/main/scala/io/prophecy/pipelines/pipelineautomationgithub1727782809869/config/Context.scala
+++
@@ -1,4 +0,0 @@
-package io.prophecy.pipelines.pipelineautomationgithub1727782809869.config
-
-import org.apache.spark.sql.SparkSession
-case class Context(spark: SparkSession, config: Config)
