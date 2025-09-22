9ff49facf6a2d68f329c84e444e0926737a793f0
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2025-09-22 16:06:31 +0000

    Add new pipeline `Workflow-Automation-ROLLBACK-1758557064084` with initial configuration and dependencies

---
+++ regression1/e2etestsmainn8_external/1758557074737/pipelines/Workflow-Automation-ROLLBACK-1758557064084/code/src/main/scala/io/prophecy/pipelines/workflowautomationrollback1758557064084/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.workflowautomationrollback1758557064084.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
---
+++ regression1/e2etestsmainn8_external/1758557074737/pipelines/Workflow-Automation-ROLLBACK-1758557064084/code/src/main/scala/io/prophecy/pipelines/workflowautomationrollback1758557064084/functions/udafs.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomationrollback1758557064084.functions
+
+import org.apache.spark.sql.expressions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+object UDAFs { def registerUDAFs(spark: SparkSession): Unit = {} }
