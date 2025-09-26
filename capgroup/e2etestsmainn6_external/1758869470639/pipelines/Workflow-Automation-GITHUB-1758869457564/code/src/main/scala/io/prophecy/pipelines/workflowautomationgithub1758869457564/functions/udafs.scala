c44e992fd341bd7c6775634dfe6b7c803b6f962a
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2025-09-26 06:54:50 +0000

    Add new pipeline Workflow-Automation-GITHUB-1758869457564 with initial configuration

---
+++ capgroup/e2etestsmainn6_external/1758869470639/pipelines/Workflow-Automation-GITHUB-1758869457564/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1758869457564/functions/udafs.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomationgithub1758869457564.functions
+
+import org.apache.spark.sql.expressions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+object UDAFs { def registerUDAFs(spark: SparkSession): Unit = {} }
