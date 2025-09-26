db83112fe6d26c1db36c67eacba189faf0e79bca
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2025-09-26 15:42:57 +0000

    Add new pipeline Workflow-Automation-GITHUB-1758901203845 with config, UDF, and main scaffolding

---
+++ regression2/e2etestsmainn7_external/1758901216721/pipelines/Workflow-Automation-GITHUB-1758901203845/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1758901203845/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomationgithub1758901203845.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
---
+++ regression2/e2etestsmainn7_external/1758901216721/pipelines/Workflow-Automation-GITHUB-1758901203845/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1758901203845/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.workflowautomationgithub1758901203845.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
