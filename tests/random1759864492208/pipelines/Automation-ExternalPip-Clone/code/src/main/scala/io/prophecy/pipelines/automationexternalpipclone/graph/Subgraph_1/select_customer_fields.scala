c6881d946cbce30e3ec04ddfe6dfa2d425a1ba8a
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2025-10-07 19:15:17 +0000

    Cloning prophecy project main branch

---
+++ tests/random1759864492208/pipelines/Automation-ExternalPip-Clone/code/src/main/scala/io/prophecy/pipelines/automationexternalpipclone/graph/Subgraph_1/select_customer_fields.scala
@@ -0,0 +1,27 @@
+package io.prophecy.pipelines.automationexternalpipclone.graph.Subgraph_1
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.automationexternalpipclone.functions.PipelineInitCode._
+import io.prophecy.pipelines.automationexternalpipclone.functions.UDFs._
+import io.prophecy.pipelines.automationexternalpipclone.graph.Subgraph_1.config.Context
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object select_customer_fields {
+
+  def apply(context: Context, in: DataFrame): DataFrame =
+    in.select(col("customer_id"),
+              col("first_name"),
+              col("last_name"),
+              col("phone"),
+              col("email"),
+              col("country_code"),
+              col("account_open_date"),
+              col("account_flags")
+    )
+
+}
