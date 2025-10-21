8226c6d7bb5a6b85bccaa344799d8b14fba5033b
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2025-10-21 04:10:39 +0000

    prophecy test commit

---
+++ cwan/e2etestsmainn6_external/1761019088476/pipelines/Pipeline-Automation-GITHUB-1761019068134/code/src/main/scala/io/prophecy/pipelines/pipelineautomationgithub1761019068134/functions/udafs.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.pipelineautomationgithub1761019068134.functions
+
+import org.apache.spark.sql.expressions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+object UDAFs { def registerUDAFs(spark: SparkSession): Unit = {} }
