39ff53f5e99104a0ebdaad9ed5857a089d3a4fa4
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2025-10-31 04:01:40 +0000

    Add new pipeline Workflow-Automation-GITHUB-1761883145388 with initial config, UDFs, and setup files

---
+++ regression/e2etestsmainn7_external/1761883155120/pipelines/Workflow-Automation-GITHUB-1761883145388/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1761883145388/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomationgithub1761883145388.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
