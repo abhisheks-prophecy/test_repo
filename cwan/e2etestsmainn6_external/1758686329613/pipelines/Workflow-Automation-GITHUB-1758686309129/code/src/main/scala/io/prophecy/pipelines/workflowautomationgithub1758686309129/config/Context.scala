a7e67fb0a206f9ba59ca97e67585ea7d198217f3
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2025-10-31 07:27:36 +0000

    Simple workflow Squash and Rebase

---
+++ cwan/e2etestsmainn6_external/1758686329613/pipelines/Workflow-Automation-GITHUB-1758686309129/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1758686309129/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.workflowautomationgithub1758686309129.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
--- uitesting/e2etestsmain5/pipelines/Workflow-Automated-GITHUB-1657539329298/code/job/graph/__init__.py
+++
@@ -1,3 +0,0 @@
-from .dataset_cust_in import dataset_cust_in
-from .dataset_out_27365 import dataset_out_27365
-from .Filter_1 import Filter_1
