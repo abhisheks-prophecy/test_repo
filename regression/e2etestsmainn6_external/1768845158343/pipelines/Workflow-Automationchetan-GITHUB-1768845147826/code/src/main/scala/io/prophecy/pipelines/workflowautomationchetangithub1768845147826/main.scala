c66b0ea04daf4be3b4fbf80817da794984f3704a
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2026-01-19 17:56:31 +0000

    Add new pipeline `Workflow-Automationchetan-GITHUB-1768845147826` with initial configuration and code files

---
+++ regression/e2etestsmainn6_external/1768845158343/pipelines/Workflow-Automationchetan-GITHUB-1768845147826/code/src/main/scala/io/prophecy/pipelines/workflowautomationchetangithub1768845147826/main.scala
@@ -0,0 +1,40 @@
+package io.prophecy.pipelines.workflowautomationchetangithub1768845147826
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.workflowautomationchetangithub1768845147826.config._
+import io.prophecy.pipelines.workflowautomationchetangithub1768845147826.functions.UDFs._
+import io.prophecy.pipelines.workflowautomationchetangithub1768845147826.functions.PipelineInitCode._
+import io.prophecy.pipelines.workflowautomationchetangithub1768845147826.graph._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Main {
+  def apply(context: Context): Unit = {}
+
+  def main(args:     Array[String]): Unit = {
+    val config = ConfigurationFactoryImpl.getConfig(args)
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("Workflow-Automationchetan-GITHUB-1768845147826")
+      .enableHiveSupport()
+      .getOrCreate()
+    val context = Context(spark, config)
+    spark.conf.set("prophecy.metadata.pipeline.uri",
+                   "pipelines/Workflow-Automationchetan-GITHUB-1768845147826"
+    )
+    spark.conf.set("spark.default.parallelism",             "4")
+    spark.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
+    registerUDFs(spark)
+    MetricsCollector.instrument(
+      spark,
+      "pipelines/Workflow-Automationchetan-GITHUB-1768845147826"
+    ) {
+      apply(context)
+    }
+  }
+
+}
