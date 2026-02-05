7ba13cdd2c2e40c7f3c02f54ed3deb0152f58063
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2026-02-05 05:56:25 +0000

    Add new pipeline `Workflow-Automation-GITHUB-1770270704538` with initial configuration and code files

---
+++ cz/e2etestsmainn6_external/1770270720181/pipelines/Workflow-Automation-GITHUB-1770270704538/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1770270704538/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomationgithub1770270704538.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
