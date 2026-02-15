dce24f4d2ef16d9799c86d690454f735de345003
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2026-02-15 23:07:31 +0000

    prophecy test commit

---
+++ regression3/e2etestsmainn6_external/1771196045835/pipelines/Pipeline-Automationstabreg-GITHUB-1771196022704/code/src/main/scala/io/prophecy/pipelines/pipelineautomationstabreggithub1771196022704/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.pipelineautomationstabreggithub1771196022704.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
