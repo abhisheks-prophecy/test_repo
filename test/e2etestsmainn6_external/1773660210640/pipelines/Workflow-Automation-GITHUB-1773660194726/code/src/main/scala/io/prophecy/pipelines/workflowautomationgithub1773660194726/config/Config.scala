fddff3f2897acc514e17975e14f639307ae684fc
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2026-03-16 11:27:34 +0000

    Add Scala Maven external pipeline Workflow-Automation-GITHUB scaffolding

---
+++ test/e2etestsmainn6_external/1773660210640/pipelines/Workflow-Automation-GITHUB-1773660194726/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1773660194726/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomationgithub1773660194726.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
