4a9c284f6334d2895ac76ab8ef86d8d909f6200e
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2026-04-21 17:47:46 +0000

    Add a new external Maven/Scala pipeline project for GitHub automation
    - Introduce a full external workflow module under regression2/e2etestsmainn6_external/1776793424152
    - Add pom.xml, Scala config (Config, Context, ConfigStore), main wiring, and UDF/UDAF scaffolds
    - Create initial resources, default workflow JSON, and placeholder workflow conf
    - Register UDFs/UDAFs and bootstrap pipeline structure for future tests
    - Add project README, pbt_project.yml, and related scaffolding files
    - Update entities to reflect a new Pipeline: Workflow-Automation-GITHUB-1776793411440

---
+++ regression2/e2etestsmainn6_external/1776793424152/pipelines/Workflow-Automation-GITHUB-1776793411440/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1776793411440/functions/udafs.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomationgithub1776793411440.functions
+
+import org.apache.spark.sql.expressions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+object UDAFs { def registerUDAFs(spark: SparkSession): Unit = {} }
