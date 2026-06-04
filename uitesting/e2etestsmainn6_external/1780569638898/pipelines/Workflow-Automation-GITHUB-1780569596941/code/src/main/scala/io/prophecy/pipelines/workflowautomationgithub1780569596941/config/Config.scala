21eec3ad27a823e053bf874c68d599cf82137a6e
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2026-06-04 10:45:21 +0000

    Add new Maven-based pipeline `Workflow-Automation-GITHUB-1780569596941` with Scala configuration, UDFs, and main wiring

---
+++ uitesting/e2etestsmainn6_external/1780569638898/pipelines/Workflow-Automation-GITHUB-1780569596941/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1780569596941/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomationgithub1780569596941.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
---
+++ uitesting/e2etestsmainn6_external/1780569638898/pipelines/Workflow-Automation-GITHUB-1780569596941/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1780569596941/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.workflowautomationgithub1780569596941.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
