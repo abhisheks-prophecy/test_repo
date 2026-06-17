34f343b9a97568e5d69dd19e6a5be8110a403dbe
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2026-06-17 04:53:40 +0000

    Add external Maven-based workflow scaffold for GitHub automation regression
    - Introduce new external pipeline project regression/e2etestsmainn6_external/1781671737207
    - Add Maven-based workflow: Workflow-Automation-GITHUB-1781671694963 with Scala config (pom.xml, main wiring, UDFs/UDAFs, config and context)
    - Add pipeline resources: default.json, workflow.conf, and associated Scala config classes (Config, Context, ConfigStore)
    - Include UDFs/UDAFs and main entrypoint for the pipeline
    - Create scaffolding: README, pbt_project.yml, and related metadata for CI readiness
    - Register new pipeline in staging as part of the external workflow project
    - File additions cover: pom.xml, default.json, workflow.conf, Config.scala, Context.scala, ConfigStore.scala, UDFs.scala, udafs.scala, main.scala, and related pbt/workflow metadata
    
    Note: This commit adds the external workflow scaffold only; no in-repo SQL/dbt changes.

---
+++ regression/e2etestsmainn6_external/1781671737207/pipelines/Workflow-Automation-GITHUB-1781671694963/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1781671694963/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomationgithub1781671694963.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
