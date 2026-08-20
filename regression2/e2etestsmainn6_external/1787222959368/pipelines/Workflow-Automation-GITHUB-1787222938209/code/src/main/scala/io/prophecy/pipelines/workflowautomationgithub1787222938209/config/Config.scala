766f4e229774e17522463c1608b2960cf380b0de
abhisheks+e2etests@prophecy.io <abhisheks+e2etests@prophecy.io>
2026-08-20 10:53:56 +0000

    Add Maven-based pipeline `Workflow-Automation-GITHUB-1787222938209` for regression testing - Introduce new external pipeline with Maven project setup - Add Scala configuration, UDFs, UDAFs, and main wiring - Include workflow resources and minimal JSON/config structure

---
+++ regression2/e2etestsmainn6_external/1787222959368/pipelines/Workflow-Automation-GITHUB-1787222938209/code/src/main/scala/io/prophecy/pipelines/workflowautomationgithub1787222938209/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.workflowautomationgithub1787222938209.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
